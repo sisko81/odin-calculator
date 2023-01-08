03c47c49a1ef4aaf3cf5b045e26c4b6448f73b43
sisko81 <74885506+sisko81@users.noreply.github.com>
2023-01-08 09:48:34 +0900

    update

--- app.js
+++ app.js
@@ -35,18 +35,34 @@ class Calculator {
       const prev = parseFloat(this.previousOperand);
       const current = parseFloat(this.currentOperand);
 
+      if(isNaN(prev) || isNaN(current)) return
+      switch(this.operation) {
+        case '+':
+            computation = prev + current
+            break
+
+        case '-':
+            computation = prev - current
+            break
+
+        case '*':
+            computation = prev * current
+            break
+            
+        case '÷':
+            computation = prev / current
+            break   
+            default:
+            return  
+      }
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.previousOperand = ''
     }
 
     updateDisplay() {
       this.currentOperandTextElement.innerText = this.currentOperand;
       this.previousOperandTextElement.innerText = this.previousOperand;
-
-      if(isNaN(prev) || isNaN(current)) return
-      switch(this.operation) {
-        case '+':
-            computation = prev + current
-      }
-
     }
 }
 
